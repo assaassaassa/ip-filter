2580db066f665e3c19d8f1780bfa4e3a7be8ba34
assaassaassa <34476753+assaassaassa@users.noreply.github.com>
2017-12-27 16:36:14 +0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -42,7 +42,7 @@ int main(int argc, char const *argv[])
             ip_pool.push_back(split(v.at(0), '.'));
         }
 
-        // TODO reverse lexicographically sort
+        // TODO reverse lexicographically sort1
 
         for(std::vector<std::vector<std::string> >::const_iterator ip = ip_pool.cbegin(); ip != ip_pool.cend(); ++ip)
         {
